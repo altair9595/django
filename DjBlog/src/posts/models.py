97ecaa80876defff6e47f6f76a064c5995f2bcda
altair9595 <mhdfadialtair@gmail.com>
2023-06-05 12:17:01 +0200

    add imags

--- DjBlog/src/posts/models.py
+++ DjBlog/src/posts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
 class post(models.Model):
     titel = models.CharField(max_length=100)
     content = models.TextField(max_length=10000)
-    
+    publish = models.BooleanField(default=False)
+    imag= models.ImageField(upload_to='posts')
+    tags = TaggableManager()
--- DjBlog/src/project/urls.py
+++ DjBlog/src/project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from posts.views import post_list , post_detail
 
@@ -25,3 +27,5 @@ urlpatterns = [
     path('blog/', post_list),
     path('blog/<int:id_post>',post_detail),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
